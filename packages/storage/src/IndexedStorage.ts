de139ef4500e842776979f967fc6040c63c8c34b
knightxv <962068006@qq.com>
2021-03-08 19:11:28 +0800

    :sparkles: indexeddb run node

--- packages/storage/src/IndexedStorage.ts
+++ packages/storage/src/IndexedStorage.ts
@@ -1,22 +1,26 @@
 import { BFChainKVH as KVH, KVHValueJSON, KVHDiffStoreValue } from "@kvh/typings";
+import "fake-indexeddb/auto";
 import { get, set, createStore, clear } from "idb-keyval";
 import { BaseStorage } from "@kvh/demo";
 
 export class IndexedStorage extends BaseStorage implements KVH.Engine.BaseStorage {
+  constructor(public dbName: string = "BFC_KVH") {
+    super();
+  }
   /**
    * @name kvh 数据库
    * @description 存储着key的最近value
    * key: u8a [keyU8a + typeFlag]
    * value: { value: u8a, maxHeight: number }
    */
-  protected kvhStore = createStore("BFC_KVH", "kvh");
+  protected kvhStore = createStore(this.dbName, "kvh");
   /**
    * @name diff 数据库
    * @description 存储着key每个高度对应上个高度的差异
    * key: u8a [keyU8a + heightU8a]
    * value: { ph: number, diff: KVH.Base.Type.Unit.Diff }
    */
-  protected diffStore = createStore("BFC_KVH_DIFF", "diff");
+  protected diffStore = createStore(this.dbName + "_DIFF", "diff");
   getKvhStoreVal(key: Uint8Array): Promise<KVHValueJSON | undefined> {
     return get<KVHValueJSON>(key, this.kvhStore);
   }
